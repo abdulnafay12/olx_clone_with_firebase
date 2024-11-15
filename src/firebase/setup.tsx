7f5032fd6fefc043836aa58bb3a861365f160a26
Muhammad Nafay <160539906+abdulnafay12@users.noreply.github.com>
2024-11-16 04:23:27 +0500

    Update setup.tsx

--- src/firebase/setup.tsx
+++ src/firebase/setup.tsx
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import {getAuth,GoogleAuthProvider} from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCC9H0TGdJUzUTX6tKjfKh-cIsCPMI18D0",
-  authDomain: "olx-clone-2878c.firebaseapp.com",
-  projectId: "olx-clone-2878c",
-  storageBucket: "olx-clone-2878c.firebasestorage.app",
-  messagingSenderId: "105088765726",
-  appId: "1:105088765726:web:357e76c04d6c37d08d430f"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 
